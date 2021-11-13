5fccec6491f92bb8fb5a8d41fa55694f2f7c12bd
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-11-13 13:50:51 -0800

    S3 3nv

--- Backend/s3.js
+++ Backend/s3.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const fs = require('fs')
 const S3 = require('aws-sdk/clients/s3')
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const bucketName = "ubereatsimagebucket"
+const region = "us-west-1"
+const accessKeyId = "AKIAZZWDMJIONBW2DJGK"
+const secretAccessKey = "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"
 
 const s3 = new S3({
   region,
