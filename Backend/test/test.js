70a3bf297694cdc9e022e55757e9a3ad8bfa7f2b
Anagha Sethuraman <anagha.sethuraman@sjsu.edu>
2021-11-14 18:41:26 -0800

    Frontend final changes

--- Backend/test/test.js
+++ Backend/test/test.js
@@ -1,95 +1,113 @@
-var assert = require("chai").assert;
-var app = require("../index");
-var supertest = require("supertest");
 
+
+var supertest = require("supertest");
+var app = require("../index");
+ var server = supertest.agent("http://localhost:5000");
 var chai = require("chai");
 chai.use(require("chai-http"));
-var expect = require("chai").expect;
-
-var agent = require("chai").request.agent(app);
-let server = require('../app');
-let should = chai.should();
-describe("UberEats", function () {
+//var supertest = require("supertest");
+//let server = require('../app');
+var assert = require('assert');
+describe('UberEats', function () {
   describe("Customer Login Test", () => {
-    it("Incorrect password", (done) => {
-       
-      chai.request(server)
-          .post('/custlogin')
-          .send({ email: "user1@gmail.com", password: "incorrect password" })
-          .end((err, res) => {
-                
-                res.body.should.have.property('message').eql('Invalid credentials');
-            done();
-          });
-    });
+  it('Incorrect password',(done) => {
+    
+    server.post("/custlogin")
+          .send({ email: "anu@gmail.com", password: "nu" })
+          .then(function (res) {
+             assert.equal(res.status, 400);
+              done();
+          })
+          .catch(done);
   });
-
-  describe("Valid User Test", () => {
-    it("Invalid User", (done) => {
-       
-      chai.request(server)
-          .post('/custlogin')
-          .send({ email: "userunknown@gmail.com", password: "incorrect password" })
-          .end((err, res) => {
-                
-                res.body.should.have.property('message').eql('Invalid User');
-            done();
-          });
-    });
+});
+describe("Valid User Test", () => {
+  it('Invalid User',(done) => {
+    
+    server.post("/custlogin")
+          .send({ email: "anvu@gmail.com", password: "nu" })
+          .then(function (res) {
+             assert.equal(res.status, 400);
+              done();
+          })
+          .catch(done);
   });
-    describe("Valid Registration Test", () => {
-      it("Existing e-mail", (done) => {
-         
-        chai.request(server)
-            .post('/register')
-            .send({ email: "user1@gmail.com", password: "incorrect password" ,username :"ana"})
-            .end((err, res) => {
-                  
-                  res.body.should.have.property('message').eql('User email already registered');
+});
+describe("Valid Registration Test", () => {
+  it('Existing e-mail',(done) => {
+    
+    server.post("/register")
+          .send({ email: "anu@gmail.com", password: "nu" })
+          .then(function (res) {
+             assert.equal(res.status,404);
               done();
-            });
-      });
-    });
-    describe("Valid Profile", () => {
-      it("Profile Exists", (done) => {
-         
-        chai.request(server)
-            .post('/getcustomerprofile')
-            .send({ userid: "1000"})
-            .end((err, res) => {
-                  res.body.should.have.property('message').eql('No Profile Exists');
+          })
+          .catch(done);
+  });
+});
+describe("Valid Profile", () => {
+  it('Profile Exists',(done) => {
+    
+    server.post("/getcustomerprofile")
+          .send({ _id:'61720800fc28fe32fcc4bafc' })
+          .then(function (res) {
+            
+             assert.equal(res.status,200);
               done();
-            });
-      });
-    });
+          })
+          .catch(done);
+  });
+});
+describe("Restaurant Dishes", () => {
+  it('View Restaurant dishes',(done) => {
+    
+    server.post("/getrestaurantwithid")
+          .send({restaurantid: "619072c60b29187784101eb5" })
+          .then(function (res) {
+            
+             assert.equal(res.status,200);
+              done();
+          })
+          .catch(done);
+  });
+});
+})
 
-    describe("Restaurant Dishes", () => {
-      it("View Restaurant dishes", (done) => {
+
+
+
+
+
+
+//   //   describe("Valid Profile", () => {
+
+//   //   describe("Restaurant Dishes", () => {
+//   //     it("View Restaurant dishes", (done) => {
          
-        chai.request(server)
-            .post('/getrestaurantdishes')
-            .send({ restaurantid: "1000"})
-            .end((err, res) => {
-                  res.body.should.have.property('message').eql('No Dishes Found');
-              done();
-            });
-      });
-    });
-    describe("View Order", () => {
-      it("View Customer Orders", (done) => {
-        chai.request(server)
-            .post('/getrestaurantorders')
-            .send({ restaurantid: "1000"})
-            .end((err, res) => {
-                  res.body.should.have.property('message').eql('No Orders Found');
-              done();
-            });
-      });
-    });
+//   //       chai.request(server)
+//   //           .post('/getrestaurantdishes')
+//   //           .send({ restaurantid: "1000"})
+//   //           .end((err, res) => {
+//   //                 res.body.should.have.property('message').eql('No Dishes Found');
+//   //             done();
+//   //           });
+//   //     });
+//   //   });
+//   //   describe("View Order", () => {
+//   //     it("View Customer Orders", (done) => {
+//   //       chai.request(server)
+//   //           .post('/getrestaurantorders')
+//   //           .send({ restaurantid: "1000"})
+//   //           .end((err, res) => {
+//   //                 res.body.should.have.property('message').eql('No Orders Found');
+//   //             done();
+//   //           });
+//   //     });
+//   //   });
     
 
 
 
-});
+// });
  
  
